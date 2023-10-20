cd72e46c7cb0e3483e4f280ad67f59f8b540f437
Li Do <lidoapps@gmail.com>
2023-10-20 22:49:42 +0700

    Update application.py

--- application.py
+++ application.py
@@ -11,9 +11,9 @@ ms_token = os.environ.get("ms_token", None)  # set your own ms_token
 
 async def fetch_trending_videos():
     async with TikTokApi() as api:
-        await TikTokApi().create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3)
+        await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3)
         trending_videos = []
-        async for video in TikTokApi().trending.videos(count=10):
+        async for video in api.trending.videos(count=10):
             trending_videos.append(video.as_dict)
         return trending_videos
     
@@ -24,7 +24,7 @@ def hello_world():
 @application.route('/get_trending')
 def get_trending():
     try:
-        trending_videos = fetch_trending_videos()
+        trending_videos = asyncio.run(fetch_trending_videos())
         return jsonify(trending_videos)
     except Exception as e:
         return jsonify({"error": str(e)})
@@ -35,4 +35,18 @@ def get_user():
     trending_videos = asyncio.run(fetch_trending_videos())
     return jsonify(trending_videos)
 
+@application.route('/run_tiktok_search')
+def run_tiktok_search():
+    ms_token = os.environ.get("ms_token", None)
+    
+    async def search_users():
+        async with TikTokApi() as api:
+            await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3)
+            results = []
+            async for user in api.search.users("david teather", count=10):
+                results.append(user)
+            return results
+    
+    results = asyncio.run(search_users())
+    return str(results)
 
