8b6557f71929e4c9991d44ada66f871bddb1e9a2
Li Do <lidoapps@gmail.com>
2023-10-20 22:41:28 +0700

    Update application.py

--- application.py
+++ application.py
@@ -11,9 +11,9 @@ ms_token = os.environ.get("ms_token", None)  # set your own ms_token
 
 async def fetch_trending_videos():
     async with TikTokApi() as api:
-        await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3)
+        await TikTokApi().create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3)
         trending_videos = []
-        async for video in api.trending.videos(count=10):
+        async for video in TikTokApi().trending.videos(count=10):
             trending_videos.append(video.as_dict)
         return trending_videos
     
